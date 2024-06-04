9d805f4b00e1f520e8e718e3212be80a939b8ee0
SonderNJU <cuihy2017@163.com>
2024-06-04 20:44:57 +0800

    TrainANN_2Layers_multibit.m

--- multibit_ANN/TrainANN_2Layers_multibit.m
+++ multibit_ANN/TrainANN_2Layers_multibit.m
@@ -1,16 +1,16 @@
-function [w1,w2,b1,b2,loss_history]=TrainANN_2Layers_multibit_real(w1,w2,b1,b2,actfunc1,actfunc2,I,O,lr,epoch,loss_history)%%bits表示权重更新的精�?
+function [w1,w2,b1,b2,loss_history]=TrainANN_2Layers_multibit_real(w1,w2,b1,b2,actfunc1,actfunc2,I,O,lr,epoch,loss_history)
 num=size(I,3);
 lossn=0;
 
 for k=1:num    
-    v1=w1*I(1,:,k)'+b1;      %将特征信息加权求和，输入隐藏层细�?
-    y1=actfunc(v1,actfunc1);  %细胞将求和结果激活并输出
+    v1=w1*I(1,:,k)'+b1;      
+    y1=actfunc(v1,actfunc1);  
 
-    v2=w2*y1+b2;      %将特征信息加权求和，输入隐藏层细�?   
+    v2=w2*y1+b2;       
    
     y=actfunc(v2,actfunc2);
 
-    e=O(k,:)-y';       %计算输出结果和正确结果的误差
+    e=O(k,:)-y';       
        
     db2=e';
     dw2=e'*y1';
@@ -23,12 +23,12 @@ for k=1:num
     w2=update_multibit(w2,dw2,lr);
     b2=update_multibit(b2,db2,lr);
     
-    loss = sqrt(sum(e.*e)); % 计算损失�?
-    lossn =lossn+loss;        % 保存损失�?
+    loss = sqrt(sum(e.*e)); 
+    lossn =lossn+loss;        
    if k==num;
        loss_history(epoch)=lossn/num;
        lossn=0;
    end
-                   %更新后的权重被输出，函数结束
+                  
 end
 end
